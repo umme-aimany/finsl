3c32c1434a76f8fb4b31738a154405fb17bf03b4
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2025-05-12 19:15:11 +0500

    Update FetchUsers.jsx

--- client/src/pages/FetchUsers.jsx
+++ client/src/pages/FetchUsers.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
-const FetchUsers = () => {
+const FetchUsers = ({refresh}) => {
   const [userData, setUserData] = useState([]);
   const [editingId, setEditingId] = useState(null);
   const [editName, setEditName] = useState("");
   const [editEmail, setEditEmail] = useState("");
+  const [searchUser, setSearchUser] = useState("");
 
-  useEffect(() => {
-    const fetchUserData = async () => {
+  const fetchUserData = async () => {
       try {
         const response = await axios.get("http://localhost:3000/getUsers");
         // console.log(response.data.users);
@@ -20,8 +20,9 @@ const FetchUsers = () => {
       }
     }
 
+  useEffect(() => {
     fetchUserData();
-  }, [userData])
+  }, [refresh])
 
   const handleDelete = async (id) => {
     const response = await axios.delete(`http://localhost:3000/deleteuser/${id}`);
@@ -35,6 +36,8 @@ const FetchUsers = () => {
 
   const handleEdit = (user) => {
     setEditingId(user._id);
+    setEditName(user.name);
+    setEditEmail(user.email);
   }
 
   const saveEdit = async () => {
@@ -53,11 +56,33 @@ const FetchUsers = () => {
     }
   }
 
+  const handleSearch = async() => {
+    if(!searchUser){
+      fetchUserData();
+      return;
+    }
+    try {
+    const response = await axios.get(`http://localhost:3000/search-user/${searchUser}`);
+    console.log(response.data);
+    setUserData(response.data);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <>
-        {userData.length > 0 ?
-
-      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 mx-[20%] my-20">
+      {userData.length > 0 ?
+        <>
+        <div className='mx-[20%] my-4'>
+            <input type="text" className='w-[100%] border border-gray-700 p-1 px-6 focus:outline-none focus:border-red-600'
+            value={searchUser}
+            onChange={(e) => setSearchUser(e.target.value)}
+            onKeyUp={handleSearch}
+            />
+        </div>
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 mx-[20%] mb-10">
 
           <table className="table">
             {/* head */}
@@ -77,7 +102,7 @@ const FetchUsers = () => {
 
                   <td>
                     {editingId == user._id ?
-                      <input className='border border-gray-600 p-2 w-[100%]' type="text" value={user.name} onChange={(e) => setEditName(e.target.value)}/>
+                      <input className='border border-gray-600 p-2 w-[100%]' type="text" value={editName} onChange={(e) => setEditName(e.target.value)} />
                       :
                       user.name
                     }
@@ -85,31 +110,32 @@ const FetchUsers = () => {
 
                   <td>
                     {editingId == user._id ?
-                      <input className='border border-gray-600 p-2 w-[100%]' type="email" value={user.email} onChange={(e) => setEditEmail(e.target.value)}/>
+                      <input className='border border-gray-600 p-2 w-[100%]' type="email" value={editEmail} onChange={(e) => setEditEmail(e.target.value)} />
                       :
                       user.email
                     }
                   </td>
                   <td className='space-x-2'>
                     {editingId == user._id ?
-                    <>
-                    <button className="btn btn-soft btn-success" onClick={saveEdit}>Save</button>
-                    <button className="btn btn-soft btn-warning" onClick={() => setEditingId(null)}>Cancel</button>
-                    </>
-                    :
-                    <>
-                    <button className="btn btn-soft btn-info" onClick={() => handleEdit(user)}>Edit</button>
-                    <button className="btn btn-soft btn-error" onClick={() => handleDelete(user._id)}>Delete</button>
-                    </>
-                  }
+                      <>
+                        <button className="btn btn-soft btn-success" onClick={saveEdit}>Save</button>
+                        <button className="btn btn-soft btn-warning" onClick={() => setEditingId(null)}>Cancel</button>
+                      </>
+                      :
+                      <>
+                        <button className="btn btn-soft btn-info" onClick={() => handleEdit(user)}>Edit</button>
+                        <button className="btn btn-soft btn-error" onClick={() => handleDelete(user._id)}>Delete</button>
+                      </>
+                    }
                   </td>
                 </tr>
               ))}
 
             </tbody>
           </table>
-      </div>
-          : null}
+        </div>
+        </>
+        : null}
 
     </>
   )
